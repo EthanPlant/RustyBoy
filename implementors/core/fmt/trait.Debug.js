ca667f63a4f9926ea3d9390651b0acea2053ecad
EthanPlant <EthanPlant@users.noreply.github.com>
2023-08-09 20:59:24 +0000

    deploy: 1e2aab01e23c080fb2b40b865a11b50935f962fd

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
-"emu":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"emu/struct.Args.html\" title=\"struct emu::Args\">Args</a>"]]
+"emu":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"emu/struct.Args.html\" title=\"struct emu::Args\">Args</a>"]],
+"gblib":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"gblib/cpu/cpu/struct.Cpu.html\" title=\"struct gblib::cpu::cpu::Cpu\">Cpu</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
