9fc58dd6cca7bca226e6e75c794514bfeadff03a
Ethan Plant <plant.ethan@gmail.com>
2023-08-11 18:38:59 -0700

    Create window

--- rustyboy/src/main.rs
+++ rustyboy/src/main.rs
@@ -1,5 +1,7 @@
+
 use clap::Parser;
 use env_logger::Env;
+use winit::{window::WindowBuilder, event_loop::{ControlFlow, EventLoop}, event::{Event, WindowEvent}};
 
 use rustyboy_core::gameboy::Gameboy;
 
@@ -13,11 +15,29 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_title("RustyBoy")
+        .with_inner_size(winit::dpi::LogicalSize::new(160, 144))
+        .build(&event_loop)
+        .unwrap();
+
     // If no log level is specified, default to info or above
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let mut gb = Gameboy::new(&args.rom);
-    loop {
+    
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
         gb.step();
-    }
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window.id() == window_id => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }
